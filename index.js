d9fcb67bf05dbaa5c96601f86a92db331905e9c3
Michael Rhoads <mrhoads14@outlook.com>
2025-11-09 22:22:00 -0500

    organize into functions

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 
-const userInput = prompt("Please enter a list of comma-separated froyo flavors:");
-const words = userInput.split(",");
-
-const order = {};
-for(let i = 0; i < words.length; i++) {
-  if(words[i] in order === false) {
-    order[words[i]] = 0;
+const storeOrder = (userInput) => {
+  const words = userInput.split(",");
+  const order = {};
+  for(let i = 0; i < words.length; i++) {
+    if(words[i] in order === false) {
+      order[words[i]] = 0;
+    }
+    order[words[i]]++;
   }
-  order[words[i]]++;
+  return order;
 }
 
-console.log("Here is the number of servings you have ordered of each flavor:")
-for(const key in order) {
-  console.log(`${key}: ${order[key]}`);
+const printOrder = (order) => {
+  console.log("Here is the number of servings you have ordered of each flavor:")
+  for(const key in order) {
+    console.log(`${key}: ${order[key]}`);
+  }
 }
+
+
+const userInput = prompt("Please enter a list of comma-separated froyo flavors:");
+
+const order = storeOrder(userInput);
+printOrder(order);
